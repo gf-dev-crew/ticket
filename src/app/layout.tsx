fc02cbe09f579b40da042ad75d5e5c57419beb62
kimzeze <fridaynight@kakao.com>
2025-05-14 18:01:40 +0900

    style: 루트 레이아웃/메인 컨텐츠를 위해 헤더만큼 윗 여백 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     <html lang='ko'>
       <body className={`${pretendard.variable} flex min-h-screen flex-col antialiased`}>
         <Header />
-        <main className='flex-1'>{children}</main>
+        <main className='pt-6xl flex-1'>{children}</main>
         <Footer />
       </body>
     </html>
