0de857fb0d1c47ec8d0760c3f16ae261b8cb2699
kimzeze <fridaynight@kakao.com>
2025-05-14 18:02:13 +0900

    design: 헤더와 푸터 임시 마크업

--- src/shared/layout/Footer/Footer.tsx
+++ src/shared/layout/Footer/Footer.tsx
@@ -1,70 +1,19 @@
-import Image from 'next/image';
 import Link from 'next/link';
+
+import Logo from '@/shared/assets/logo_white.svg';
 
 export default function Footer() {
   return (
-    <footer className='flex min-h-[300px] w-full items-center border-t border-gray-200 xl:h-[250px]'>
-      <div className='px-xl mx-auto flex w-full max-w-[1600px] flex-col'>
-        {/* 메인 콘텐츠 */}
-        <div className='flex flex-col gap-6 py-8 xl:flex-row xl:justify-between'>
-          {/* 좌측: 로고와 슬로건 */}
-          <div className='flex flex-col items-start justify-between xl:h-[150px]'>
-            <Image
-              src='/assets/logo.svg'
-              alt='로고'
-              width={200}
-              height={60}
-              className='mb-4 -translate-x-5 xl:mb-0 xl:-translate-x-2'
-            />
-            <div className='xl:pl-sm flex flex-col items-start text-xl xl:text-2xl'>
-              <p className='font-medium text-gray-400'>임시 슬로건</p>
-              <p>
-                <span className='font-medium'>임시 문구입니다. </span>수정이 필요합니다.
-              </p>
-            </div>
-          </div>
-
-          {/* 우측: 회사 정보 */}
-          <div className='flex w-full flex-col gap-2 text-sm text-gray-700 xl:h-[150px] xl:w-[700px]'>
-            <div className='xl:mb-xl xl:gap-2xl mb-6 flex flex-col gap-3 xl:flex-row'>
-              <Link href='#' className='text-gray-700 hover:text-gray-900'>
-                임시 링크 1
-              </Link>
-              <Link href='#' className='text-gray-700 hover:text-gray-900'>
-                임시 링크 2
-              </Link>
-              <Link href='#' className='text-gray-700 hover:text-gray-900'>
-                임시 링크 3
-              </Link>
-              <Link href='#' className='text-gray-700 hover:text-gray-900'>
-                임시 링크 4
-              </Link>
-            </div>
-
-            <div className='flex flex-col gap-3 xl:flex-row xl:items-center xl:gap-8'>
-              <div className='xl:gap-xs flex flex-col xl:flex-row xl:items-center'>
-                <span className='font-medium text-gray-800'>주소</span>
-                <span>임시 주소</span>
-              </div>
-              <div className='xl:gap-xs flex flex-col xl:flex-row xl:items-center'>
-                <span className='font-medium text-gray-800'>전화</span>
-                <span>000-0000-0000</span>
-              </div>
-              <div className='xl:gap-xs flex flex-col xl:flex-row xl:items-center'>
-                <span className='font-medium text-gray-800'>이메일</span>
-                <span>temp@example.com</span>
-              </div>
-            </div>
-            <div className='xl:mb-xl mb-6 flex flex-col xl:flex-row xl:items-center xl:gap-8'>
-              <div className='xl:gap-xs flex flex-col xl:flex-row xl:items-center'>
-                <span className='font-medium text-gray-800'>사업자등록번호</span>
-                <span>000-00-00000</span>
-              </div>
-            </div>
-            <div className='text-gray-300 xl:mt-2'>
-              Copyright © 2024 Company Name. All rights reserved.
-            </div>
-          </div>
+    <footer className='pt-6xl pb-4xl flex h-[300px] w-full items-center justify-center bg-[#1f1d1e] text-white'>
+      <div className='max-w-contents flex h-full w-full flex-row justify-between border border-pink-500'>
+        <div className='flex h-full w-full items-center justify-between border border-white'>
+          <Logo className='h-[45px] w-[160px] object-contain' alt='Nook Brand Logo' />
+        </div>
+        <div className='px-xl mx-auto flex h-full w-full items-center justify-between border border-white'>
+          <Logo className='h-[45px] w-[160px] object-contain' alt='Nook Brand Logo' />
+        </div>
+        <div className='px-xl mx-auto flex h-full w-full items-center justify-between border border-white'>
+          <Logo className='h-[45px] w-[160px] object-contain' alt='Nook Brand Logo' />
         </div>
       </div>
     </footer>
--- src/shared/layout/Header/Header.tsx
+++ src/shared/layout/Header/Header.tsx
@@ -1,27 +1,51 @@
-import Image from 'next/image';
 import Link from 'next/link';
+
+import Logo from '@/shared/assets/logo.svg';
 
 export default function Header() {
   return (
-    <header className='fixed top-0 right-0 left-0 z-99 w-full bg-white/10 text-black backdrop-blur-sm'>
-      <div className='mx-auto flex h-[64px] items-center justify-between px-[24px]'>
-        <Link href='/' className='flex items-center justify-center'>
-          <Image src='/assets/logo.svg' alt='Nook Brand Logo' width={160} height={45} priority />
-        </Link>
-        <nav>
-          <ul className='text-md flex space-x-8'>
-            <li>
-              <Link
-                href='/'
-                className='hover:cursor-pointer hover:text-gray-600'
-                target='_blank'
-                rel='noopener noreferrer'
-              >
-                고객센터
-              </Link>
-            </li>
-          </ul>
-        </nav>
+    <header className='h-6xl text-md text-dark fixed top-0 right-0 left-0 z-99 w-full border-b bg-white/10 font-medium backdrop-blur-sm'>
+      <div className='max-w-contents px-xl mx-auto flex h-full w-full items-center justify-between'>
+        {/* 로고와 메인 네비게이션 영역 */}
+        <div className='space-x-5xl flex items-center'>
+          <Link href='/' className='flex items-center justify-center'>
+            <Logo className='h-[45px] w-[160px] object-contain' alt='Nook Brand Logo' />
+          </Link>
+
+          {/* 메인 네비게이션 */}
+          <nav>
+            <ul className='space-x-4xl flex'>
+              <li>
+                <Link href='/notice' className='transition-colors hover:text-gray-600'>
+                  공지사항
+                </Link>
+              </li>
+              <li>
+                <Link href='/calendar' className='transition-colors hover:text-gray-600'>
+                  캘린더
+                </Link>
+              </li>
+              <li>
+                <Link href='/program' className='transition-colors hover:text-gray-600'>
+                  프로그램
+                </Link>
+              </li>
+              <li>
+                <Link href='/lounge' className='transition-colors hover:text-gray-600'>
+                  라운지
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+        {/* 사용자 영역 */}
+        <div className='space-x-2xl flex items-center'>
+          <p className='text-gray-200'>🏡 힐스테이트 에코 마곡나루</p>
+          <Link href='/login' className='transition-colors hover:text-gray-600'>
+            <p className='font-medium'>로그인 / 회원가입</p>
+          </Link>
+        </div>
       </div>
     </header>
   );
