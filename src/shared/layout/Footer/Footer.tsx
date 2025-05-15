53ae309e9c4df5b62b8170a03754d364fd1397e2
kimzeze <fridaynight@kakao.com>
2025-05-15 17:55:09 +0900

    style: 푸터 레이아웃 및 텍스트 스타일 개선 - 저작권 및 개발자 정보 추가

--- src/shared/layout/Footer/Footer.tsx
+++ src/shared/layout/Footer/Footer.tsx
@@ -1,19 +1,33 @@
-import Link from 'next/link';
-
 import Logo from '@/shared/assets/logo_white.svg';
 
 export default function Footer() {
   return (
-    <footer className='pt-6xl pb-4xl flex h-[300px] w-full items-center justify-center bg-[#1f1d1e] text-white'>
-      <div className='max-w-contents flex h-full w-full flex-row justify-between border border-pink-500'>
-        <div className='flex h-full w-full items-center justify-between border border-white'>
+    <footer className='pt-6xl pb-4xl flex h-[300px] w-full items-center justify-center bg-[#1f1d1e] text-gray-400'>
+      <div className='max-w-contents px-xl mx-auto flex h-full w-full flex-row justify-between'>
+        <div className='flex h-full w-full flex-col'>
           <Logo className='h-[45px] w-[160px] object-contain' alt='Nook Brand Logo' />
+          <p className='mt-2xs'>Copyright ⓒ 2025 Kzz-Dev-Crew. All rights reserved.</p>
         </div>
-        <div className='px-xl mx-auto flex h-full w-full items-center justify-between border border-white'>
-          <Logo className='h-[45px] w-[160px] object-contain' alt='Nook Brand Logo' />
+        <div className='px-xl mx-auto flex h-full w-full'>
+          <p className='flex w-full flex-col'>
+            <span className='mb-sm flex justify-start text-lg font-semibold text-white'>
+              Developers
+            </span>
+            <span className='text-sm font-semibold text-gray-300'>Kim Dohyeon</span>
+            <span className='text-sm'>Email. fridaynight@kakao.com</span>
+            <span className='text-sm'>GitHub. https://github.com/kimzeze</span>
+            <span className='mt-sm text-sm font-semibold text-gray-300'>Hyeon Jisoo</span>
+            <span className='text-sm'>Email. hyunzsu@kakao.com</span>
+            <span className='text-sm'>GitHub. https://github.com/hyunzsu</span>
+          </p>
         </div>
-        <div className='px-xl mx-auto flex h-full w-full items-center justify-between border border-white'>
-          <Logo className='h-[45px] w-[160px] object-contain' alt='Nook Brand Logo' />
+        <div className='px-xl mx-auto flex h-full w-full items-center justify-between'>
+          <span>
+            <p>
+              <span>Address. </span>
+              <span>161-11 Magokjungang-ro, Gangseo-gu, Seoul Magok</span>
+            </p>
+          </span>
         </div>
       </div>
     </footer>
