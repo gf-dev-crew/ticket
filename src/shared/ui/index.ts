3be15c54d1c209478b939d345ab091d624dea022
kimzeze <fridaynight@kakao.com>
2025-05-20 14:34:27 +0900

    feat: 공통 UI 컴포넌트에 BaseModal 및 Portal 추가, 스크롤 유틸리티 함수 내보내기

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -2,3 +2,5 @@
  * 공통 UI 컴포넌트 내보내기
  */
 export { default as Button } from './Button/Button';
+export { default as BaseModal } from './BaseModal/BaseModal';
+export { default as Portal } from './Portal/Portal';
